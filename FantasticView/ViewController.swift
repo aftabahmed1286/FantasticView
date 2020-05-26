f754af9342d98ed1e8e2c2fbec5c9c6c0c8a4d14
Aftab Ahmed <aahmed03@in.ibm.com>
2020-05-26 15:19:26 +0530

    Initial Commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Aftab Ahmed on 26/05/2020.
+//  Copyright © 2020 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
